793011d34818dba536bdd558e3bbb1b706e82366
ali <159713281+cunkin375@users.noreply.github.com>
2026-05-17 22:53:44 -0400

    add windowhandle abstraction

---
+++ src/Backend/WindowHandle.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+struct GLFWwindow;
+
+namespace Backend {
+    /**
+     * @brief WindowHandle wrapper meant to support multiple APIS. Currently supports GLFW.
+    */
+    class WindowHandle {
+    private:
+        void* handle = nullptr;
+
+    public:
+        // Prevent implicit conversions from compiler
+        explicit WindowHandle(void* h) : handle(h) {}
+
+        // template method for type access
+        template<typename T>
+        T* as() const { return static_cast<T*>(handle); }
+
+        GLFWwindow* asGLFW() const { return as<GLFWwindow>(); }
+        
+        // For use in "if (!WindowHandle) {...}" checks
+        explicit operator bool() const { return handle != nullptr; }
+        
+
+    };
+}
