a48cfc1a46628495fd3ca30b046d529e6f65dc75
ali <159713281+cunkin375@users.noreply.github.com>
2026-05-22 15:41:21 -0400

    add GL backend init function

--- src/API/OpenGL/GL_backend.cpp
+++ src/API/OpenGL/GL_backend.cpp
@@ -1,7 +1,28 @@
 #include "GL_backend.hpp"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 namespace OpenGLBackend {
     void Init()
+    {
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cerr << "Failed to initialize GLAD\n";
+            return;
+        }
+        GLint major, minor;
+        glGetIntegerv(GL_MAJOR_VERSION, &major);
+        glGetIntegerv(GL_MINOR_VERSION, &minor);
+        const GLubyte* vendor = glGetString(GL_VENDOR);
+        const GLubyte* renderer = glGetString(GL_RENDERER);
+        std::cout << "GPU: " << renderer << "\n";
+        std::cout << "OpenGL vendor: " << vendor << "\n";
+        std::cout << "OpenGL version: " << major << "." << minor << "\n";
+    }
+
+    void BeginFrame()
     {
     }
 }
--- src/API/OpenGL/GL_backend.hpp
+++ src/API/OpenGL/GL_backend.hpp
@@ -2,5 +2,6 @@
 
 namespace OpenGLBackend {
     void Init();
+    void BeginFrame();
 }
 
