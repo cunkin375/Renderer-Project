cc14f6dfd0f2a323c999b0c3d2c956a236fa2674
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-14 13:24:00 -0400

    add abreviated types for math vectors

--- src/App/Types.hpp
+++ src/App/Types.hpp
@@ -10,7 +10,18 @@ using i64 = std::int64_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
+// Should never happen but kept incase of stupid edits / end of the world
+static_assert(sizeof(float)  == 4, "App/Types.hpp requires floats to be 4 bytes (32 bits)!!!");
+static_assert(sizeof(double) == 8, "App/Types.hpp requires doubles to be 8 bytes (64 bites)!!!");
+
+using f32 = float;
+using f64 = double;
+
 using ivec2 = Math::Vector<2zu, i32>;
+using ivec3 = Math::Vector<3zu, i32>;
+
+using vec2  = Math::Vector<2zu, f32>;
+using vec3  = Math::Vector<3zu, f32>;
 
 struct Resolution {
     ivec2 size;
