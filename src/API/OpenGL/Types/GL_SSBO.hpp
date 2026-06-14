f7b7652e2c8598fcef9c92c513c5697c7b7696e6
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-14 08:11:54 -0400

    implement OpenGL SSBO type
    squasm me

--- src/API/OpenGL/Types/GL_SSBO.hpp
+++ src/API/OpenGL/Types/GL_SSBO.hpp
@@ -1,6 +1,33 @@
 #pragma once
 
+#include "App/Types.hpp"
+
+#include <glad/glad.h>
+
+using ObjectHandle = u32;
+using ObjectBuffer = std::size_t;
+
 // TODO: implement this
 struct OpenGLSSBO {
+private:
+    ObjectHandle handle_ = 0;
+    ObjectBuffer buffer_size_ = 0;
+    GLbitfield flags_bitmask_ = 0;
+
+public:
+    OpenGLSSBO() = default;
+    OpenGLSSBO(ObjectBuffer size, GLbitfield flags_bitmask);
+
+    void Reserve(ObjectBuffer size);
+    void Update(ObjectBuffer size, const void* data);
+    void UploadStatic(ObjectBuffer size, const void* data);
+    void Bind(ObjectHandle index) const;
+    void CleanUp();
+    void CopyFrom(const void* host_pointer, std::size_t size_in_bytes);
+    void Clear() const;
+    void ClearRange(std::size_t offset, ObjectBuffer size);
+
+    const ObjectHandle GetHandle() const { return handle_; }
+    const ObjectBuffer GetSize() const { return buffer_size_; }
 
 };
