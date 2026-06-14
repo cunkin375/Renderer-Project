23bd8bca49b85d8546730e9c266a5713dd8e790f
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-14 10:03:11 -0400

    begin implementing math library
    squash me

--- src/App/Types.hpp
+++ src/App/Types.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "glm/glm.hpp"
+#include "Math/IntegerVector.hpp"
+#include "Math/FloatVector.hpp"
+
+#include <cstdint>
 
 using i32 = std::int32_t;
 using i64 = std::int64_t;
@@ -8,6 +11,12 @@ using i64 = std::int64_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
+using ivec =
+
 struct Resolution {
-    glm::ivec2 size;
+    Math::IntegerVector2 size;
+};
+
+struct Vertex {
+
 };
---
+++ src/Math/IntegerVector.hpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+namespace Math {
+
+// here we go
+struct IntegerVector2 {
+    std::int32_t x;
+    std::int32_t y;
+};
+
+struct IntegerVector3 {
+    std::int32_t x;
+    std::int32_t y;
+    std::int32_t z;
+};
+
+}
