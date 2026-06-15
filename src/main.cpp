349466dbbde425ddc1750894aacb54212ec9fe58
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-15 14:16:26 -0400

    refactor: change main.cpp styling

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
-#include "Define/Enums.hpp"
 #include "Backend/Backend.hpp"
+#include "Define/Enums.hpp"
 #include "Renderer/Renderer.hpp"
 
-int main() 
-{
+int main() {
     std::cout << "Pre-Window\n";
 
-    if (!Backend::Init(API::OPENGL, WindowMode::WINDOWED))
-    {
+    if (!Backend::Init(API::OPENGL, WindowMode::WINDOWED)) {
         std::cerr << "Backend failed to initialize!\n";
         std::exit(-1);
     }
 
-    while (Backend::WindowIsOpen())
-    {
+    while (Backend::WindowIsOpen()) {
         Backend::BeginFrame();
         Backend::Update();
         Renderer::Render();
@@ -25,5 +22,4 @@ int main()
     Backend::Destroy();
 
     std::cout << "Hello, world!\n";
-
 }
