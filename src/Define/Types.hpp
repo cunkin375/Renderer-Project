fb30ddba55c42c7751e0d7997f6d3c8631a8449b
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-20 12:53:03 -0400

    format Types.hpp and remove aliases inclusion

--- src/Define/Types.hpp
+++ src/Define/Types.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
 #include "Math/Vector.hpp"
-#include "Util/Aliases.hpp"
 
 struct Resolution {
     ivec2 size;
 };
 
-struct Vertex {
+struct VertexData {
     vec3 position{};
 
-    Vertex(f32 x, f32 y, f32 z) : position{ vec3{x, y, z} } {}
+    VertexData(float x, float y, float z) : position{vec3{x, y, z}} {}
 
-    Vertex(const vec3& _position) : position{ _position } {}
-
+    VertexData(const vec3 &_position) : position{_position} {}
 };
