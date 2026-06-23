5a0adb4139f04540ef13a82cfe7a9c3be27cfbc3
ali <159713281+cunkin375@users.noreply.github.com>
2026-06-22 22:03:06 -0400

    save GL_MeshBuffer code before attempted fix

--- src/API/OpenGL/Types/GL_MeshBuffer.cpp
+++ src/API/OpenGL/Types/GL_MeshBuffer.cpp
@@ -1,33 +1,50 @@
 #include "GL_MeshBuffer.hpp"
 
+#include "ResourceHandling/Types/Common.hpp"
 #include "Util/AutoRelease.hpp"
+
+namespace {
+    using NumVAOs = GLsizei;
+    using NumVBOs = GLsizei;
+    using NumEBOs = GLsizei;
+
+    using Index = GLuint;
+    using BindingIndex = Index;
+    using AttributeIndex = Index;
+
+    using Offset = GLintptr;
+    using Stride = GLsizei;
+    using Size = GLint;
+} // namespace
 
 OpenGLMeshBuffer::OpenGLMeshBuffer(const ModelData &data)
     :  vao_{
-        []()-> GLuint { auto vao = GLuint{}; glCreateVertexArrays(1, &vao); return vao; }(), 
-        [](auto vao) { glDeleteVertexArrays(1, &vao); } 
+        []()-> GLuint { auto vao = GLuint{}; glCreateVertexArrays(NumVAOs{1}, &vao); return vao; }(), 
+        [](auto vao) { glDeleteVertexArrays(NumVAOs{1}, &vao); } 
+
     }, vbo_{
-        []()-> GLuint { auto vbo = GLuint{}; glCreateBuffers(1, &vbo); return vbo; }(), 
-        [](auto vbo) { glDeleteBuffers(1, &vbo); }
+        []()-> GLuint { auto vbo = GLuint{}; glCreateBuffers(NumVBOs{1}, &vbo); return vbo; }(), 
+        [](auto vbo) { glDeleteBuffers(NumVBOs{1}, &vbo); }
+
     }, ebo_{
-        []()-> GLuint { auto ebo = GLuint{}; glCreateBuffers(1, &ebo); return ebo; }(), 
-        [](auto ebo) { glDeleteBuffers(1, &ebo); }
-    }, vertex_stride_{sizeof(f32) * 3}
+        []()-> GLuint { auto ebo = GLuint{}; glCreateBuffers(NumEBOs{1}, &ebo); return ebo; }(), 
+        [](auto ebo) { glDeleteBuffers(NumEBOs{1}, &ebo); }
+
+    }, vertex_stride_{sizeof(VertexData)}
 {
-    glBindVertexArray(vao_);
+    glNamedBufferStorage(vbo_, data.vertices.size_bytes(), data.vertices.data(), GL_STATIC_DRAW);
+    glNamedBufferStorage(ebo_, data.indices.size_bytes(), data.indices.data(), GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_);
-    glBufferData(GL_ARRAY_BUFFER, data.vertices.size_bytes(), data.vertices.data(), GL_STATIC_DRAW);
+    glVertexArrayVertexBuffer(vao_, BindingIndex{0}, vbo_, Offset{0}, Stride{vertex_stride_});
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, data.indices.size_bytes(), data.indices.data(), GL_STATIC_DRAW);
+    glVertexArrayElementBuffer(vao_, ebo_);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
-    glEnableVertexAttribArray(0);
+    // Position attribute
+    glEnableVertexArrayAttrib(vao_, Index{0});
+    glVertexArrayAttribFormat(vao_, AttributeIndex{0}, Size{3}, GL_FLOAT, GL_FALSE, offsetof(VertexData, position));
+    glVertexArrayAttribBinding(vao_, AttributeIndex{0}, BindingIndex{0});
 
-    glBindVertexArray(0);
 }
-
 
 void OpenGLMeshBuffer::Bind() { glBindVertexArray(vao_); }
 
