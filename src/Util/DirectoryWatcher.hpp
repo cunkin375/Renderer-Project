bc8e86109cddc93b813a89c615f2ce9519d9179b
Ali <159713281+cunkin375@users.noreply.github.com>
2026-06-26 21:42:21 -0400

    add DirectoryWatcher header

---
+++ src/Util/DirectoryWatcher.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <filesystem>
+#include <functional>
+
+class DirectoryWatcher {
+    enum class FileAction {
+        MODIFIED,
+        CREATED,
+        DELETED,
+        RENAMED_FROM,
+        RENAMED_TO
+    };
+
+    struct FileEvent {
+        FileAction action;
+        std::filesystem::path filepath;
+    };
+
+    using Callback = std::function<void(const FileEvent &)>;
+
+    constexpr explicit DirectoryWatcher(const std::filesystem::path &watch_dir,
+                                        Callback on_change,
+                                        bool recursive = true);
+
+    ~DirectoryWatcher();
+
+    // No copies
+    DirectoryWatcher(const DirectoryWatcher &other) = delete;
+    DirectoryWatcher &operator=(const DirectoryWatcher &other) = delete;
+    // Movable
+    DirectoryWatcher(DirectoryWatcher &&other) noexcept;
+    DirectoryWatcher &operator=(DirectoryWatcher &&other) noexcept;
+
+    void PollEvents();
+
+    void SetEnabled(bool enabled);
+    bool IsEnabled() const;
+
+private:
+    struct Implementation;
+    std::unique_ptr<Implementation> impl_;
+};
